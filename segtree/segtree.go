40cf0fc40e78a591cdf4355cca7b6f1ce35e36b7
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-09-22 20:44:29 +0200

    Implement Segtree.

--- segtree/segtree.go
+++ segtree/segtree.go
@@ -11,18 +11,72 @@ type EOf[S any] interface {
 type Segtree[S any, Op OpOf[S], E EOf[S]] struct {
 	op Op
 	e  E
+	d  []S
+}
+
+func (s *Segtree[_, _, _]) leaf(i int) int {
+	return i + (len(s.d) >> 1)
+}
+
+func isRight(i int) bool {
+	return (i & 1) == 0
+}
+
+func parent(i int) int {
+	return (i - 1) >> 1
 }
 
 func (s *Segtree[S, _, _]) Set(p int, x S) {
+	i := s.leaf(p)
+	for {
+		s.d[i] = x
+		if i == 0 {
+			break
+		}
+		j := i
+		if isRight(i) {
+			j--
+		} else {
+			j++
+		}
+		x = s.op(x, s.d[j])
+		i = parent(i)
+	}
+}
+
+func (s *Segtree[S, _, _]) Get(p int) S {
+	return s.Prod(p, p+1)
 }
 
 func (s *Segtree[S, _, _]) Prod(l, r int) S {
-	return s.e()
+	l, r = s.leaf(l), s.leaf(r)
+	v := s.e()
+	for l < r {
+		if isRight(l) {
+			v = s.op(v, s.d[l])
+			l++
+		}
+		l = parent(l)
+		if isRight(r) {
+			v = s.op(v, s.d[r-1])
+		}
+		r = parent(r)
+	}
+	return v
 }
 
-func New[S any, Op OpOf[S], E EOf[S]](op Op, e E) *Segtree[S, Op, E] {
+func New[S any, Op OpOf[S], E EOf[S]](op Op, e E, n int) *Segtree[S, Op, E] {
+	two := 1
+	for two < n {
+		two <<= 1
+	}
+	d := make([]S, two*2-1)
+	for i := 0; i < len(d); i++ {
+		d[i] = e()
+	}
 	return &Segtree[S, Op, E]{
 		op: op,
 		e:  e,
+		d:  d,
 	}
 }
--- segtree/segtree_test.go
+++ segtree/segtree_test.go
@@ -5,7 +5,23 @@ import "testing"
 func TestSegtree(t *testing.T) {
 	op := func(a, b int) int { return a + b }
 	e := func() int { return 0 }
-	s := New(op, e)
-	s.Set(0, 1)
-	s.Prod(2, 3)
+	s := New(op, e, 10)
+
+	for i := 0; i < 10; i++ {
+		s.Set(i, i+1)
+	}
+	for i := 0; i < 10; i++ {
+		want := i + 1
+		if got := s.Get(i); got != want {
+			t.Errorf("s.Get(%d) = %d; want %d", i, got, want)
+		}
+	}
+	for l := 0; l < 10; l++ {
+		for r := l; r <= 10; r++ {
+			want := (r + l + 1) * (r - l) / 2
+			if got := s.Prod(l, r); got != want {
+				t.Errorf("s.Prod(%d, %d) = %d; want %d", l, r, got, want)
+			}
+		}
+	}
 }
